90f241c099aae339073522917161b2be7adb0c59
bgswe <bg.soft.eng@gmail.com>
2024-12-13 18:12:55 -0700

    esa-23: user registration handler test

--- test/unit/handlers/user/test_user_registration.py
+++ test/unit/handlers/user/test_user_registration.py
@@ -1,34 +1,38 @@
-# from unittest.mock import AsyncMock, MagicMock
-# from uuid import uuid4
+from unittest.mock import MagicMock
+from uuid import uuid4
 
-# import pytest
+import pytest
 
-# from bookt_domain.model.command_handlers import handle_register_user
-# from bookt_domain.model.commands import RegisterUser
-# from bookt_domain.model.user_registrar import UserRegistrar
-
-# pytest_plugins = ("pytest_asyncio",)
+from bookt_domain.model.command_handlers import handle_register_user
+from bookt_domain.model.commands import RegisterUser
+from bookt_domain.model.user_registrar import UserRegistrar, UserRoles
 
 
-# @pytest.mark.asyncio
-# async def test_register_user_handler():
-#     uow = MagicMock()
-#     uow.repository = MagicMock()
-#     uow.repository.get = AsyncMock()
-#     uow.repository.save = AsyncMock()
+@pytest.mark.asyncio
+async def test_user_registration_handler_is_success(mock_uow):
+    mock_registrar = MagicMock()
+    mock_uow.repository.get_singleton.return_value = mock_registrar
 
-#     mock_registrar = UserRegistrar()
-#     mock_registrar.create(tenant_id=uuid4())
-#     uow.repository.get.return_value = mock_registrar
+    mock_tenant_id = uuid4()
+    mock_user_id = uuid4()
+    mock_email = "some_email@example.com"
 
-#     await handle_register_user(
-#         unit_of_work=uow,
-#         command=RegisterUser(
-#             user_registrar_id=mock_registrar.stream_id,
-#             email="some_email",
-#             password="some_password",
-#         ),
-#     )
+    await handle_register_user(
+        unit_of_work=mock_uow,
+        command=RegisterUser(
+            tenant_id=mock_tenant_id,
+            user_id=mock_user_id,
+            email=mock_email,
+        ),
+    )
 
-#     uow.repository.get.assert_called_once_with(id=mock_registrar.stream_id)
-#     uow.repository.save.assert_called_once_with(aggregate=mock_registrar)
+    mock_uow.repository.get_singleton.assert_called_once_with(
+        aggregate_root_class=UserRegistrar
+    )
+    mock_registrar.register_user.assert_called_once_with(
+        tenant_id=mock_tenant_id,
+        user_id=mock_user_id,
+        email=mock_email,
+        roles=[UserRoles.TENANT_USER],
+    )
+    mock_uow.repository.save.assert_called_once_with(aggregate=mock_registrar)
