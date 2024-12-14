65ab6412868c4b2a6322bbda1222aa905584fba1
bgswe <bg.soft.eng@gmail.com>
2024-12-13 17:47:43 -0700

    esa-23: create handler test for email validation/eligible tenants

--- test/conftest.py
+++ test/conftest.py
@@ -9,12 +9,33 @@ pytest_plugins = ("pytest_asyncio",)
 
 
 @pytest.fixture
-def mock_uow():
-    uow = MagicMock()
-    uow.repository = MagicMock()
-    uow.repository.get_singleton = AsyncMock()
-    uow.repository.save = AsyncMock()
-    return uow
+def make_mock_uow():
+    def make(
+        get_singleton_return_value: MagicMock | None = None,
+        get_return_value: MagicMock | None = None,
+    ):
+        uow = MagicMock()
+        uow.repository = MagicMock()
+        uow.repository.get = AsyncMock()
+        uow.repository.get_singleton = AsyncMock()
+        uow.repository.save = AsyncMock()
+
+        if get_singleton_return_value is None:
+            get_singleton_return_value = MagicMock()
+
+        if get_return_value is None:
+            get_return_value = MagicMock()
+
+        uow.repository.get_singleton.return_value = get_singleton_return_value
+        uow.repository.get.return_value = get_return_value
+        return uow
+
+    return make
+
+
+@pytest.fixture
+def mock_uow(make_mock_uow):
+    return make_mock_uow()
 
 
 @pytest.fixture
