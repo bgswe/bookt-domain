6a81c20ee8a530f690f095e9ad6ab7111fe1085a
bgswe <bg.soft.eng@gmail.com>
2024-12-27 12:37:52 -0700

    esa-55: save validation key to created event

--- src/bookt_domain/model/tenant_email_validator.py
+++ src/bookt_domain/model/tenant_email_validator.py
@@ -5,6 +5,7 @@ from cosmos.domain import AggregateRoot, DomainEvent
 
 class TenantEmailValidatorWasCreated(DomainEvent):
     tenant_id: UUID
+    validation_key: str
 
 
 class TenantEmailWasValidated(DomainEvent):
@@ -23,10 +24,13 @@ class TenantEmailValidator(AggregateRoot):
             self._apply_email_was_validated(event=event)
 
     def create(self, tenant_id: UUID):
+        stream_id = uuid4()
+
         self.mutate(
             TenantEmailValidatorWasCreated(
-                stream_id=uuid4(),
+                stream_id=stream_id,
                 tenant_id=tenant_id,
+                validation_key=f"{stream_id}.{uuid4().hex}",
             )
         )
 
@@ -34,8 +38,8 @@ class TenantEmailValidator(AggregateRoot):
         self._initialize(
             id=event.stream_id,
             tenant_id=event.tenant_id,
+            validation_key=event.validation_key,
             is_validated=False,
-            validation_key=f"{event.stream_id}.{'SOME_KEY'}",
         )
 
     def validate_email(self, validation_key: str):
