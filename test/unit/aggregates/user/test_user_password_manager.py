12e868513ca8b4c54eb1ff1d56b27deb9227ed11
bgswe <bg.soft.eng@gmail.com>
2024-12-31 17:52:55 -0700

    esa-67: final coverage

--- test/unit/aggregates/user/test_user_password_manager.py
+++ test/unit/aggregates/user/test_user_password_manager.py
@@ -45,4 +45,26 @@ def test_user_set_password_key_is_invalid(password_manager):
         manager.set_password(key="OBVIOUSLY WRONG KEY", password="PASSWORD")
 
 
-# def test_user_password_manager
+def test_set_password_adds_password_hash_to_password_history(password_manager):
+    manager, _ = password_manager
+
+    assert len(manager.password_history) == 0
+    manager.set_password(
+        key=manager.initial_password_key,
+        password="some_password",
+    )
+
+    assert len(manager.password_history) == 1
+
+
+def test_set_initial_password_sets_initial_key_to_none(password_manager):
+    manager, _ = password_manager
+
+    assert manager.initial_password_key is not None
+
+    manager.set_password(
+        key=manager.initial_password_key,
+        password="some_password",
+    )
+
+    assert manager.initial_password_key is None
